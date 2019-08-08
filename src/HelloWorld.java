4fbdb80cf926177fc293c74c4ab606d980dafddb
vphimpha <vphimpha@montgomerycollege.edu>
2019-08-08 15:31:59 -0400

    first program

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is July 19, 2015. I am alive!");
+        System.out.println("MY name is Victor Phimphachanh and I will be your instructor for these 8-weeks.");
+    }
+}
